712158b5e2f0fba3d5eaaace017bc95e32e046ac
Chia213 <chiaranchber@gmail.com>
2025-02-25 11:12:07 +0100

    removed Home button and replaced it with FitExplorer as button which navigates to main page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaHome, FaUser, FaInfoCircle } from "react-icons/fa";
+import { FaUser, FaInfoCircle } from "react-icons/fa";
 import logo from "../assets/logo.png";
 
 function Navbar() {
@@ -9,23 +9,18 @@ function Navbar() {
   return (
     <header className="w-full bg-gradient-to-r from-blue-400 to-emerald-400 text-black flex justify-between items-center py-6 px-8 md:px-32 drop-shadow-md">
       <div className="flex items-center gap-4">
-        <Link to="/">
+        <Link to="/" className="flex items-center gap-2">
           <img
             src={logo}
             alt="Logo"
             className="w-20 hover:scale-105 transition-all"
           />
+          <span className="text-xl font-bold cursor-pointer">FitExplorer</span>
         </Link>
-        <span className="text-xl font-bold">FitExplorer</span>
       </div>
+
       <div className="flex items-center gap-8">
         <ul className="flex items-center gap-6 font-semibold text-base">
-          <li className="flex flex-col items-center p-3 hover:bg-sky-700 hover:text-white rounded-md transition-all cursor-pointer">
-            <Link to="/" className="flex flex-col items-center">
-              <FaHome size={24} />
-              <span className="text-sm">Home</span>
-            </Link>
-          </li>
           <li className="flex flex-col items-center p-3 hover:bg-sky-700 hover:text-white rounded-md transition-all cursor-pointer">
             <Link to="/about" className="flex flex-col items-center">
               <FaInfoCircle size={24} />
