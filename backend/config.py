f3d8c355be8c0dab949f8f5f4b823704642a020f
Chia213 <chiaranchber@gmail.com>
2025-03-03 17:08:18 +0100

    token works now in postman and other requests

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,8 @@ import os
 
 load_dotenv()
 
-DB_URL = os.getenv("DB_URL")
-SECRET_KEY = os.getenv("SECRET_KEY")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15))
+DB_URL = os.getenv(
+    "DB_URL", "postgresql+psycopg2://postgres:idioten@localhost:5432/fitdemo")
+SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 1000000))
