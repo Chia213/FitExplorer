45cda9d9d003c22d1fed2d2306efb17b2ea8a71d
Chia213 <chiaranchber@gmail.com>
2025-03-03 17:26:07 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from database import engine, Base, get_db
 from auth import router as auth_router
@@ -9,6 +10,14 @@ from schemas import WorkoutCreate
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 
