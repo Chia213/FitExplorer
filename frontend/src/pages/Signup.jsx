50b5b0361f18b304ef0e71718ab58f944226607b
Chia213 <chiaranchber@gmail.com>
2025-03-04 12:48:12 +0100

    Signup small fixes

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -131,7 +131,7 @@ function Signup() {
             <button
               type="button"
               onClick={togglePasswordVisibility}
-              className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 dark:text-gray-400"
+              className="absolute top-12 right-3 transform -translate-y-1/2 text-gray-500 dark:text-gray-400"
             >
               {showPassword ? <FaEyeSlash size={20} /> : <FaEye size={20} />}
             </button>
@@ -156,7 +156,7 @@ function Signup() {
             <button
               type="button"
               onClick={toggleConfirmPasswordVisibility}
-              className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 dark:text-gray-400"
+              className="absolute top-12 right-3 transform -translate-y-1/2 text-gray-500 dark:text-gray-400"
             >
               {showConfirmPassword ? (
                 <FaEyeSlash size={20} />
