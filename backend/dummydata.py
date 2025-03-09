16bc83ed752e56c1de5d4258d4265115cea0f700
Chia Ranchber <chiaranchber@gmail.com>
2025-03-09 01:15:56 +0100

    it works now with migrations its updated with the latest models.py

--- backend/dummydata.py
+++ backend/dummydata.py
@@ -1,12 +1,9 @@
-# create_tables.py
 from database import Base, engine
 from models import User, UserPreferences, Workout, Exercise, Set, CustomExercise, Routine
 
 def create_tables():
-    # Force drop existing tables if they exist
     Base.metadata.drop_all(bind=engine)
     
-    # Create all tables
     Base.metadata.create_all(bind=engine)
     print("All tables created successfully!")
 
