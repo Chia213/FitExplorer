ea0260ca91e25c433dda49a8ec2acdb42bc7f50a
Chia Ranchber <chiaranchber@gmail.com>
2025-04-08 13:12:41 +0200

    New implementaiton prep for deploy added qr code

---
+++ frontend/public/serviceWorker.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'fitexplorer-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Install service worker and cache static assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate service worker and clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+// Fetch from network or cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            // Skip caching if not GET request
+            if (!event.request.url.startsWith('http') || event.request.method !== 'GET') {
+              return response;
+            }
+            
+            // Clone the response
+            let responseToCache = response.clone();
+            
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+              
+            return response;
+          })
+          .catch(() => {
+            // Return fallback if network fails and content isn't cached
+            if (event.request.mode === 'navigate') {
+              return caches.match('/');
+            }
+          });
+      })
+  );
+}); 
